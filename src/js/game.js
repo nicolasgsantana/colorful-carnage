f37d85d9d6a7a6b9fe2052e27a6ef2d3e4a501e1
Nicorovisk <nicolas07san@gmail.com>
2023-05-27 16:41:44 -0300

    projectiles add

--- src/js/game.js
+++ src/js/game.js
@@ -14,11 +14,56 @@ class Player {
     }
 
     draw() {
+        ctx.beginPath()
         ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true)
         ctx.fillStyle = this.color
         ctx.fill()
     }
 }
 
+class Projectile {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+        this.velocity = velocity
+    }
+
+    draw() {
+        ctx.beginPath()
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true)
+        ctx.fillStyle = this.color
+        ctx.fill()
+    }
+
+    update() {
+        this.draw()
+        this.x += this.velocity.x
+        this.y += this.velocity.y
+    }
+}
+
 const player = new Player(canvas.width / 2, canvas.height / 2, 30, 'blue')
-player.draw()
+
+const projectiles = []
+
+function animate() {
+    requestAnimationFrame(animate)
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    projectiles.forEach(projectile => {
+        projectile.update()
+    })
+    player.draw()
+}
+
+window.addEventListener('click', (event) => {
+    const angle = Math.atan2(event.clientY - canvas.height / 2, event.clientX - canvas.width / 2)
+    const velocity = {
+        x: Math.cos(angle),
+        y: Math.sin(angle)
+    }
+    projectiles.push(new Projectile(canvas.width / 2, canvas.height / 2, 5, 'red', velocity))
+})
+
+animate()
